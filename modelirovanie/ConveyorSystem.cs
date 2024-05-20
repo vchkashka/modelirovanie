e64bb67ffb593c24bc7dda8b9af4d4ab79d24551
ekaterinatoporova04@mail.ru <ekaterinatoporova04@mail.ru>
2024-05-21 00:12:46 +0700

    Добавьте файлы проекта.

---
+++ modelirovanie/ConveyorSystem.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modelirovanie
+{
+
+    public class Buffer
+    {
+        public double T_osv { get; set; } // время когда станок будет освобожден
+
+        public Buffer()
+        {
+            T_osv = 0;
+        }
+
+    }
+
+    public class Machine // класс станка
+    {
+        public int Index { get; set; }
+
+        public int ServicedDetail { get; set; } // количество обслуженных деталей
+        public double T_osv { get; set; } // время когда станок будет освобожден
+
+        public Buffer[] Buffers { get; set; } // массив буферов
+
+
+        public Machine(int index, int sizeBuffer)
+        {
+            Index = index;
+            T_osv = 0;
+            ServicedDetail = 0;
+            Buffers = new Buffer[sizeBuffer];
+            for (int i = 0; i < Buffers.Length; i++)
+            {
+                Buffers[i] = new Buffer();
+            }
+
+        }
+    }
+
+    public class ConveyorSystem
+    {
+
+        public int Lyamda { get; set; } // интенчивность прихода заявки
+
+        public int SimulationTime { get; set; } // время симуляции прихода заявок (в минутах)
+
+
+        public Machine[] Machines { get; set; } // станки
+
+
+
+        public ConveyorSystem(int diveceCount, int sizeBuffer, int lyamda, int simulationTime)
+        {
+            this.Lyamda = lyamda;
+            this.SimulationTime = simulationTime;
+
+            Machines = new Machine[diveceCount];
+
+            for (int i = 0; i < Machines.Length; i++)
+            {
+                Machines[i] = new Machine(i + 1, sizeBuffer);
+
+            }
+
+        }
+
+        public (double, double, double, double, double) Simulate()
+        {
+            double N_obs_sr = 0, N_neobs_sr = 0, T_pr = 0 , T_obr = 0, K = 0;
+            double t_prich = 0; // время прихода
+
+            double t_osv = 0; // время освобождения заявки
+
+            int k = 0; // счетчик деталей
+
+            int N_obs = 0; // счетчик обслуженных деталей
+
+
+
+            double T_obs = 0; // время обслуживания
+
+            double T_ob = 0;
+
+            Random random = new Random();
+
+
+            while (t_prich < SimulationTime)
+            {
+                bool flag = false;
+
+                t_prich += arrivalTime(random, Lyamda);
+                k++;
+                //Console.WriteLine($"Заявка № {k} пришла в {t_prich*60} секунд");
+
+                foreach (Machine machine in Machines)
+                {
+                    if (flag)
+                    {
+                        break;
+                    }
+                    if (t_prich < machine.T_osv)
+                    {
+                        //Console.WriteLine($"Станок № {machine.Index} занят, будет свободен в  {machine.T_osv * 60 + (machine.Index - 1)} секунд");
+                        foreach (Buffer buffer in machine.Buffers)
+                        {
+                            if (t_prich < buffer.T_osv)
+                            {
+                                //Console.WriteLine($"Буфер станка № {machine.Index} занят");
+                            }
+                            else
+                            {
+                                //Console.WriteLine($"Буфер  станка № {machine.Index} свободен");
+                                double t_nach = machine.T_osv;
+                                //Console.WriteLine($"Время начала обслуживания заявки № {k} - {t_nach * 60} секунд, время в пути до станка {(machine.Index - 1) * 60}");
+                                double t_obs = arrivalTime(random, 1);
+
+                                T_obs += t_obs;
+                                //Console.WriteLine($"Время обслуживания заявки № {k} - {t_obs * 60} секунд");
+                                t_osv = t_nach + t_obs;
+                                buffer.T_osv = t_osv;
+                                machine.T_osv = t_osv;
+
+                                T_ob += (t_obs + (buffer.T_osv - machine.T_osv));
+
+
+
+                                //Console.WriteLine($"Заявка № {k} обслужена в {t_osv * 60} секунд Станком № {machine.Index}\n");
+                                N_obs++;
+                                machine.ServicedDetail++;
+                                flag = true;
+                                break;
+
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //Console.WriteLine($"Станок № {machine.Index} свободен");
+                        double t_nach = t_prich + (machine.Index - 1);
+                        //Console.WriteLine($"Время начала обслуживания заявки № {k} - {t_nach * 60} секунд, время в пути до станка {(machine.Index-1) * 60}");
+                        double t_obs = arrivalTime(random, 1);
+                        T_ob += (t_obs + (machine.Index - 1));
+
+                        T_obs += t_obs;
+                        //Console.WriteLine($"Время обслуживания заявки № {k} - {t_obs * 60} секунд");
+                        t_osv = t_nach + t_obs;
+                        machine.T_osv = t_osv;
+                        //Console.WriteLine($"Заявка № {k} обслужена в {t_osv * 60} секунд Станком № {machine.Index}\n");
+                        N_obs++;
+                        machine.ServicedDetail++;
+                        break;
+
+                    }
+                }
+                //Console.WriteLine();
+
+            }
+
+            Console.WriteLine("Статистика:");
+            Console.WriteLine($"Количество станков: {Machines.Length}");
+            Console.WriteLine($"Количество места в буфере: {Machines.Last().Buffers.Length}");
+            Console.WriteLine($"Количество заявок: {k}");
+            Console.WriteLine($"Количество обслуженных заявок: {N_obs}");
+            Console.WriteLine($"Количество необслуженных заявок: {k - N_obs}");
+
+
+            foreach (Machine machine1 in Machines)
+            {
+                Console.WriteLine($"Количество обработанных деталей станком № {machine1.Index}: {machine1.ServicedDetail}");
+                K += machine1.ServicedDetail;
+            }
+            Console.WriteLine($"Cреднее время обслуживания заявок: {T_obs / N_obs * 60} секунд");
+            Console.WriteLine($"Cреднее время провожления заявок: {T_ob / N_obs * 60} секунд");
+            N_obs_sr += N_obs;
+            N_neobs_sr += k - N_obs;
+            T_pr += T_ob / N_obs * 60;
+            T_obr += T_obs / N_obs * 60;            
+            return (N_obs_sr, N_neobs_sr, T_pr, T_obr, K);
+            Console.ReadLine();
+
+        }
+
+        public double arrivalTime(Random random, double lyamda)
+        {
+
+            return -Math.Log(1 - random.NextDouble()) / lyamda;
+        }
+
+
+        public void HZ(int N, double epsilon)
+        {
+
+        }
+
+
+    }
+}
---
+++ modelirovanie/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modelirovanie
+{
+    
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double N_obs_sr = 0, N_neobs_sr = 0, T_pr = 0, T_obr = 0, K = 0;
+            double D1 = 0, D2 = 0, D3 = 0, D4 = 0, D5 = 0;
+            for (int i = 0; i < 50; i++)
+            {
+                ConveyorSystem conveyorSystem = new ConveyorSystem(5, 0, 4, 240); // количество станков, места в буфере, интенсивность прихода заявок, время симуляции прихода заявок в минутах
+
+                (double, double, double, double, double) tuple = conveyorSystem.Simulate();
+                //N_obs_sr += tuple.Item1 / 50;
+                //N_neobs_sr += tuple.Item2 / 50;
+                //T_pr += tuple.Item3 / 50;
+                //T_obr += tuple.Item4 / 50;
+                //K += tuple.Item5 / 50;
+
+                D1 += (tuple.Item1 - 463.26) * (tuple.Item1 - 463.26);
+                D2 += (tuple.Item2 - 502.78) * (tuple.Item2 - 502.78);
+                D3 += (tuple.Item3 - 133.98) * (tuple.Item3 - 133.98);
+                D4 += (tuple.Item4 - 59.64) * (tuple.Item4 - 59.64);
+               // D5 += (tuple.Item5 - 463.26) * (tuple.Item5 - 463.26);
+            }
+
+            //Console.WriteLine("N_obs_sr =" + N_obs_sr);
+            //Console.WriteLine("N_neobs_sr =" + N_neobs_sr);
+            //Console.WriteLine("T_pr =" + T_pr);
+            //Console.WriteLine("T_obr =" + T_obr);
+            //Console.WriteLine("K =" + K); 
+            
+            Console.WriteLine("D1 =" + D1/50);
+            Console.WriteLine("D2 =" + D2 / 50);
+            Console.WriteLine("D3 =" + D3 / 50);
+            Console.WriteLine("D4 =" + D4 / 50);
+           // Console.WriteLine("K =" + D5 / 50);
+            Console.ReadKey();
+        }
+    }
+}
